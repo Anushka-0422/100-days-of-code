574327da27ee75b7382a9b5ef0a7b6553e802d74
Anushka Bhosale <bhosaleanushka519@gmail.com>
2025-07-17 01:26:42 +0530

    36 th added..!

---
+++ Day 36th-JS 9/style.js
@@ -0,0 +1,70 @@
+
+
+let delhi ={
+ latitude : 28.7041,
+    longitude : 77.1025,
+}
+
+const student ={  //const maintained sequence of declaration
+    name : "Anushka",//stores momory address
+    age : 20,
+    marks : 94.80
+}
+
+let student2= {  //let not maintained sequence of declaration
+    name :"Sukanya",
+    age :21,
+    marks :95.00
+}
+
+const item = {
+    price : 100.78,
+    discount : 10,
+    colors : "red"
+}
+
+const item2 = {
+    username : "anushka_123",
+    content : "these is my first post",
+    likes : 1299,
+    repost : 5,
+    tags : ["rohan","Avi","Ashu"]
+}
+
+
+
+
+
+
+const students ={
+
+    Rohan :{
+        grade: "A+",
+        city : "Patoda"
+    },
+
+    Anu :{
+        grade : "B",
+        city :"Mumbai"
+    },
+
+    Avi :{
+        grade : "A",
+        city : "Delhi"
+    }
+}
+
+const classInfo = [
+    {
+        name : "Anu",
+        grade : "A+"
+    },
+    {
+        name : "Rohan",
+        grade : "A"
+    },
+    {
+        name : "Avi",
+        grade : "B+"
+    }
+]
