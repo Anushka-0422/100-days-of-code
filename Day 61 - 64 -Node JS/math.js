a5ec4d3fb77fd556bb5565b6f60e6f9a9fff1986
Anushka Bhosale <bhosaleanushka519@gmail.com>
2025-08-16 03:08:25 +0530

    Added next days..!

---
+++ Day 61 - 64 -Node JS/math.js
@@ -0,0 +1,4 @@
+module.exports.sum = (a , b)=>a + b;
+module.exports.mul = (a , b)=>a * b;
+module.exports.g= 9.8;
+module.exports.pi= 3.14;
---
+++ Day 61 - 64 -Node JS/script.js
@@ -0,0 +1,6 @@
+const info = require("./Fruits");
+
+console.log(info);
+console.log(info[0].color);
+console.log(info[1]);
+console.log(info[0].name);
